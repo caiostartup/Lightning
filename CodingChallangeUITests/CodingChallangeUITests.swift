092a8d4006dede2c7554f6b9e6bfa14204bbaf85
Caio Mansho <caiomansho@gmail.com>
2024-07-07 23:59:47 -0300

    test: add ui tests

--- CodingChallangeUITests/CodingChallangeUITests.swift
+++ CodingChallangeUITests/CodingChallangeUITests.swift
@@ -17,44 +17,45 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-        
+
 
 import XCTest
 import SwiftUI
 
 final class CodingChallangeUITests: XCTestCase {
-
+    
     private var app: XCUIApplication!
     
     override func setUpWithError() throws { //Does this each time it starts the test
-         continueAfterFailure = false
-         app = XCUIApplication() // Initializes the XCTest app
-         app.launch() // Launches the app
-         
+        continueAfterFailure = false
+        app = XCUIApplication() // Initializes the XCTest app
+        app.launch() // Launches the app
+        
     }
     
     override func tearDownWithError() throws { //Does this each time it ends the test
-         app = nil //Makes sure that the test wont have residual values, it will be torn down each time the funcion has finished
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app = nil //Makes sure that the test wont have residual values, it will be torn down each time the funcion has finished
     }
     
-    
-    func testSuggestedDynamicsTitleDisplay() { //Checks if the title is being presented
-         // Use the XCUIElementQuery to locate the title Text
-         let suggestedDynamicsTitleText = app.staticTexts["withAppTitle"]
-         
-         // Check if the title text is visible
-         XCTAssertTrue(suggestedDynamicsTitleText.exists)
+    func testTitleDisplay() { //Checks if the title is being presented
+        let titleText = app.staticTexts["Lightning"]
+        XCTAssertTrue(titleText.exists)
+        
+        let navigationBar = app.navigationBars["Lightning"]
+        XCTAssertTrue(navigationBar.exists)
     }
     
-
+    func testList() { //Checks if the title is being presented
+        // Use the XCUIElementQuery to locate the title Text
+        let collectionViewTexts = app.collectionViews["Sidebar"].staticTexts
+        
+        // Check if the title text is visible
+        XCTAssertTrue(collectionViewTexts["Binance"].exists)
+        XCTAssertTrue(collectionViewTexts["ACINQ"].exists)
+        XCTAssertTrue(collectionViewTexts["bfx-lnd0"].exists)
+        XCTAssertTrue(collectionViewTexts["Kraken 🐙⚡"].exists)
+    }
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
